16a9a90958410771f6e010d46d3465b9aae0d083
dominik-air <domzurek@student.agh.edu.pl>
2024-03-24 09:31:29 +0100

    update Article component to be data driven

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,27 +1,59 @@
 import Article from "./components/Article.tsx";
 
+const articleData = {
+  title: "Exploring React with TypeScript and MUI",
+  content: [
+    {
+      type: "text",
+      text: "React, TypeScript, and Material-UI (MUI) offer a powerful combination for building robust and visually appealing web applications. This article explores how to leverage these technologies effectively.",
+    },
+    {
+      type: "image",
+      imageURL:
+        "https://www.wikihow.com/images_en/thumb/2/20/Enunciate-Step-7-Version-3.jpg/v4-460px-Enunciate-Step-7-Version-3.jpg.webp",
+    },
+    {
+      type: "text",
+      text: "Tables are an important element for displaying structured data. Here's an example of how to render a table in React using Material-UI:",
+    },
+    {
+      type: "table",
+      table: {
+        headers: ["Name", "Type", "Default"],
+        data: [
+          ["useEffect", "Hook", "N/A"],
+          ["useState", "Hook", "undefined"],
+          ["Container", "Component", "N/A"],
+        ],
+      },
+    },
+    {
+      type: "image",
+      imageURL:
+        "https://www.wikihow.com/images_en/thumb/2/20/Enunciate-Step-7-Version-3.jpg/v4-460px-Enunciate-Step-7-Version-3.jpg.webp",
+    },
+    {
+      type: "text",
+      text: "Integrating TypeScript with React allows developers to write safer code with less bugs. TypeScript's static typing adds a layer of reliability that's invaluable during development.",
+    },
+  ],
+  tags: ["React", "TypeScript", "MUI", "Web Development"],
+};
+
 function App() {
   return (
     <div
-      style={{
-        position: "absolute",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
-      }}
+    // style={{
+    //   position: "absolute",
+    //   left: "50%",
+    //   top: "50%",
+    //   transform: "translate(-50%, -50%)",
+    // }}
     >
       <Article
-        topic="Article"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse gravida massa eget sodales tempor. Pellentesque vulputate elit quis mauris tempor, eu sagittis eros tincidunt. Maecenas quis rutrum lorem. Nullam facilisis eros vitae fermentum vehicula. Nullam eu vulputate metus. Nam gravida sodales nibh feugiat efficitur. Morbi eget metus eu purus porta ultrices. Nunc sollicitudin, augue ut suscipit semper, lorem odio sollicitudin mi, fermentum sagittis risus odio eu urna. Nulla pretium mollis sapien. In nec enim ex. Integer aliquam facilisis nunc, nec suscipit dui facilisis vel. Integer non mauris magna.
-
-Proin scelerisque tincidunt erat nec gravida. Duis hendrerit lectus quis est vehicula, vel sollicitudin ante viverra. Quisque vitae ex eros. Maecenas rutrum neque a libero ultrices, sit amet fermentum velit dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec accumsan viverra justo tempus ornare. Proin dolor nulla, pulvinar ac enim bibendum, malesuada elementum nisl. Mauris justo ante, luctus et rhoncus a, volutpat at est. Maecenas eu rutrum nisl. Suspendisse mollis faucibus malesuada. Etiam risus dui, pellentesque vel lacus ac, luctus consectetur enim. Mauris mollis diam in ipsum placerat auctor. Ut tempus molestie ante, in molestie diam facilisis eget. Maecenas luctus pharetra scelerisque. Vivamus id condimentum mauris, quis consectetur erat.
-
-Aliquam vehicula est in mauris pulvinar, vitae eleifend velit tincidunt. Vestibulum accumsan quam at eros ullamcorper fermentum. Fusce hendrerit eleifend nisi vel porttitor. Quisque bibendum dapibus elit vehicula varius. Sed ut laoreet urna. Aenean ac consectetur lorem. In molestie, purus eget venenatis hendrerit, tortor felis vestibulum justo, sit amet varius tortor odio sit amet est. Donec aliquam sit amet augue sed eleifend. Quisque posuere eleifend quam id ultricies. Mauris imperdiet justo et enim porta commodo. Morbi eget enim nisl. Nullam nisl odio, tempor id semper vestibulum, tristique sed ex. Proin dictum, metus ac venenatis tempus, libero nibh tincidunt erat, eu mattis ligula nisl et ipsum. Etiam tempor congue blandit.
-
-Quisque elementum rhoncus ligula, sagittis varius sapien accumsan in. Nulla facilisi. Sed justo metus, pulvinar pulvinar turpis sed, dapibus iaculis purus. Etiam consequat, ex sed convallis fringilla, sapien est eleifend velit, id tempus magna turpis et odio. Aliquam vestibulum augue nec mi auctor, vel lacinia purus fermentum. Cras ac maximus diam, nec imperdiet magna. Aliquam erat volutpat. Phasellus eget pharetra libero. Integer luctus ligula eu nisi mattis pretium. Nulla facilisi.
-
-Morbi mattis risus non dignissim lobortis. Morbi commodo mi ut tortor efficitur, id ultricies purus volutpat. Ut vel neque quis sapien facilisis tincidunt vel sed augue. Donec ut commodo magna. Sed metus nunc, tristique quis finibus sed, convallis eu ipsum. Proin eu nisl eu elit vehicula tincidunt sit amet a nisl. Nulla lorem ipsum, malesuada eu ante vitae, malesuada tristique libero. Etiam ut sapien sed nunc aliquet pharetra."
-        tags={["lorem", "ipsum", "ArticuLearn"]}
+        title={articleData.title}
+        content={articleData.content}
+        tags={articleData.tags}
       />
     </div>
   );
--- ui/src/components/Article.tsx
+++ ui/src/components/Article.tsx
@@ -7,33 +7,99 @@ import {
   Container,
   Paper,
   Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableContainer,
 } from "@mui/material";
 
+interface TableData {
+  headers: string[];
+  data: (string | number)[][];
+}
+
+interface Content {
+  type: "text" | "image" | "table";
+  text?: string;
+  imageURL?: string;
+  table?: TableData;
+}
+
 interface ArticleProps {
-  topic: string;
-  content: string;
+  title: string;
+  content: Content[];
   tags: string[];
 }
 
-const Article: React.FC<ArticleProps> = ({ topic, content, tags }) => {
+const Article: React.FC<ArticleProps> = ({ title, content, tags }) => {
+  const renderContent = (contentItem: Content, index: number) => {
+    switch (contentItem.type) {
+      case "text":
+        return (
+          <Typography key={index} variant="body1">
+            {contentItem.text}
+          </Typography>
+        );
+      case "image":
+        return contentItem.imageURL ? (
+          <img
+            key={index}
+            src={contentItem.imageURL}
+            alt=""
+            style={{
+              width: "100%",
+              maxHeight: "400px",
+              objectFit: "cover",
+              padding: 4,
+            }}
+          />
+        ) : null;
+      case "table":
+        return contentItem.table ? (
+          <TableContainer component={Paper} sx={{ padding: 2, gap: 2 }}>
+            <Table key={index}>
+              <TableHead>
+                <TableRow>
+                  {contentItem.table.headers.map((header, idx) => (
+                    <TableCell key={idx}>{header}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {contentItem.table.data.map((row, rowIndex) => (
+                  <TableRow key={rowIndex}>
+                    {row.map((cell, cellIndex) => (
+                      <TableCell key={cellIndex}>{cell}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : null;
+      default:
+        return null;
+    }
+  };
+
   return (
     <Container
       component={Paper}
       sx={{
         padding: 4,
         gap: 4,
-        maxWidth: "md",
-        margin: "auto",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
       }}
     >
       <Grid container justifyContent={"center"} alignItems={"center"}>
-        <Typography variant="h1">{topic}</Typography>
-        <Divider />
-        <Typography variant="body1">{content}</Typography>
-        <Divider />
+        <Typography variant="h3">{title}</Typography>
+        <Divider sx={{ width: "100%", my: "2vh" }} />
+        {content.map(renderContent)}
+        <Divider sx={{ width: "100%", my: "2vh" }} />
         <Stack direction={"row"} spacing={2}>
           {tags.map((tag) => (
             <Chip label={tag}></Chip>
