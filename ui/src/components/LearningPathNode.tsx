ca1aa10769a77c48f0e6d270fbb53683b8cb4f9c
dominik-air <domzurek@student.agh.edu.pl>
2024-04-02 10:52:35 +0200

    add animation that highlights the current node

--- ui/src/components/LearningPathNode.tsx
+++ ui/src/components/LearningPathNode.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import {
   Paper,
-  Container,
   Typography,
   Button,
   Box,
   SvgIcon,
+  useTheme,
 } from "@mui/material";
 import StarIcon from "@mui/icons-material/Star";
+import { styled, keyframes } from "@mui/system";
+import { PaperProps } from "@mui/material/Paper";
+
+interface AnimatedPaperProps extends PaperProps {
+  current?: boolean;
+}
+
+const createColorTransition = (
+  primaryColor: string,
+  secondaryColor: string,
+) => keyframes`
+  0% {
+    box-shadow: 0 0 8px 2px ${primaryColor};
+  }
+  50% {
+    box-shadow: 0 0 8px 2px ${secondaryColor};
+  }
+  100% {
+    box-shadow: 0 0 8px 2px ${primaryColor};
+  }
+`;
 
 interface LearningPathNodeProps {
   id: number;
@@ -24,6 +45,39 @@ const LearningPathNode = ({
   current,
   finished,
 }: LearningPathNodeProps) => {
+  const theme = useTheme();
+
+  const colorTransition = React.useMemo(
+    () =>
+      createColorTransition(
+        theme.palette.primary.main,
+        theme.palette.secondary.main,
+      ),
+    [theme.palette.primary.main, theme.palette.secondary.main],
+  );
+
+  const AnimatedPaper = styled(
+    ({ current, ...otherProps }: AnimatedPaperProps) => (
+      <Paper {...otherProps} />
+    ),
+  )<AnimatedPaperProps>(({ theme, current }) => ({
+    padding: theme.spacing(4),
+    width: 300,
+    height: 150,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    position: "relative",
+    ...(current
+      ? {
+          animation: `${colorTransition} 2s infinite alternate`,
+        }
+      : {}),
+  }));
+
   let buttonAction: string;
 
   if (unlocked && finished) {
@@ -36,22 +90,7 @@ const LearningPathNode = ({
 
   return (
     <Box position="relative" display="inline-flex" id={`node-${id}`}>
-      <Container
-        component={Paper}
-        sx={{
-          padding: 4,
-          width: 300,
-          height: 150,
-          maxWidth: "none",
-          margin: "auto",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: 2,
-          marginBottom: 2,
-          position: "relative",
-        }}
-      >
+      <AnimatedPaper current={current}>
         <Typography>{name}</Typography>
         <Button
           data-testid="button-learning-path-node"
@@ -61,7 +100,7 @@ const LearningPathNode = ({
         >
           {buttonAction}
         </Button>
-      </Container>
+      </AnimatedPaper>
       <SvgIcon
         data-testid="star-icon"
         component={StarIcon}
