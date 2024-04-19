62f9decf334c0fd097bad76c600bbc45249c2eb1
dominik-air <domzurek@student.agh.edu.pl>
2024-04-19 17:01:29 +0200

    add schemas for Quizes

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -9,3 +10,23 @@ class Video(Base):
     id = Column(String, primary_key=True, unique=True)
     title = Column(String)
     description = Column(String)
+
+
+class Quiz(Base):
+    __tablename__ = "quizes"
+
+    id = Column(Integer, primary_key=True, unique=True)
+    question = Column(String)
+
+    answers = relationship("QuizAnswer", back_populates="quiz")
+
+
+class QuizAnswer(Base):
+    __tablename__ = "quiz_answers"
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+    is_correct = Column(Boolean, default=False)
+    quiz_id = Column(Integer, ForeignKey("quizes.id"))
+
+    quiz = relationship("Quiz", back_populates="answers")
--- app/schemas.py
+++ app/schemas.py
@@ -15,3 +15,26 @@ class Video(VideoBase):
 
     class Config:
         orm_mode = True
+
+
+class QuizAnswerBase(BaseModel):
+    text: str
+    is_correct: bool
+    quiz_id: int
+
+
+class QuizAnswerCreate(QuizAnswerBase):
+    pass
+
+
+class QuizAnswer(QuizAnswerBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class QuizBase(BaseModel):
+    id: int
+    question: str
+    answers: list[QuizAnswer]
