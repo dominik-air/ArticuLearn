059df539826f78328c6ccb8a5b1a18b4de2c5905
dominik-air <domzurek@student.agh.edu.pl>
2024-05-01 18:59:03 +0200

    (majowka) add Article models

--- app/src/models.py
+++ app/src/models.py
@@ -30,3 +30,37 @@ class QuizAnswer(Base):
     quiz_id = Column(Integer, ForeignKey("quizes.id"))
 
     quiz = relationship("Quiz", back_populates="answers", lazy="subquery")
+
+
+class Article(Base):
+    __tablename__ = "articles"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+
+    tags = relationship("ArticleTag", back_populates="article", lazy="subquery")
+    contents = relationship("ArticleContent", back_populates="article", lazy="subquery")
+
+
+class ArticleTag(Base):
+    __tablename__ = "article_tags"
+
+    id = Column(Integer, primary_key=True)
+    value = Column(String)
+    article_id = Column(Integer, ForeignKey("articles.id"))
+
+    article = relationship("Article", back_populates="tags", lazy="subquery")
+
+
+class ArticleContent(Base):
+    __tablename__ = "article_contents"
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String, nullable=True)
+    image_url = Column(String, nullable=True)
+    # table = ...
+
+    article = relationship("Article", back_populates="contents", lazy="subquery")
+
+
+# TODO: add Table
